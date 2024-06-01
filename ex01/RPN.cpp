468c1d606d1f7943801c1a73088841884cf32047
Valerie St-Pierre <valerie@valeries-mbp.home>
2024-06-01 08:35:05 -0400

    ex01

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -1,89 +1,134 @@
 #include <stack>
+#include <iostream>
+#include <string>
+#include <cstring>
 
-void createstack(std::list<int> &stck, char*rpneq)
+int createstack(std::stack<int> &stck, char*rpneq)
 {
-	//TODO changer pour prendre une string "5 3 +"
-	for( int i = 0; rpneq[i]; i++)
+	int numbers = 0;
+	for( int i = strlen(rpneq) - 1; i>= 0; i--)
 	{
-		int numbers = 0;
-		if(rpneq[i] >= '0' && rpneq[i][0] <= '9')
+		if(rpneq[i] >= '0' && rpneq[i] <= '9')
 		{
-			lst.push(rpneq[i][0] - 48)
+			stck.push(rpneq[i] - 48);
 			numbers++;
 		}
-		else if(rpneq[i] == '+')
-			lst.push('+')
-		else if(rpneq[i] == '-')
-			lst.push('-')
-		else if(rpneq[i] == '/')
-			lst.push('/')
-		else if(rpneq[i] == '*')
-			lst.push('*')
+		else if(rpneq[i] == '+' || rpneq[i] == '-' || rpneq[i] == '*' || rpneq[i] == '/')
+			stck.push(rpneq[i]);
 		else if(rpneq[i] == ' ')
 			continue;
 		else
-			std::cout << "Invalid input" << std::endl;
-		if(!(rpneq[0] >= '0' && rpneq[i][0] <= '9'))
-			std::cout << "Invalid input" << std::endl;
-		if(rnpeq[i + 1] == '\0' && rpneq[i] >= '0' && rpneq[i][0] <= '9')
-			std::cout << "Invalid input" << std::endl;
-		if(rnpeq[i + 1] == '\0' && numbers != (i / 2) + 1)
-			std::cout << "Invalid input" << std::endl;
-	}
-}
-
-void executernp(std::list<int> &stck)
-{
-	int nb1;
-	int nb2;
-	int operator;
-	for( int i = 0; rpneq[i]; i++)
-	{
-		int numbers = 0;
-		if(rpneq[i] >= '0' && rpneq[i][0] <= '9')
 		{
-			lst.push(rpneq[i][0] - 48)
-			numbers++;
+			std::cout << rpneq[i] << " is an invalid value" << std::endl;
+			return(1);
 		}
-		else if(rpneq[i] == '+')
-			lst.push('+')
-		else if(rpneq[i] == '-')
-			lst.push('-')
-		else if(rpneq[i] == '/')
-			lst.push('/')
-		else if(rpneq[i] == '*')
-			lst.push('*')
-		else if(rpneq[i] == ' ')
-			continue;
-		else
-			std::cout << "Invalid input" << std::endl;
-		if(!(rpneq[0] >= '0' && rpneq[i][0] <= '9'))
-			std::cout << "Invalid input" << std::endl;
-		if(rnpeq[i + 1] == '\0' && rpneq[i] >= '0' && rpneq[i][0] <= '9')
-			std::cout << "Invalid input" << std::endl;
-		if(rnpeq[i + 1] == '\0' && numbers != (i / 2) + 1)
-			std::cout << "Invalid input" << std::endl;
 	}
-
-	
+	if(!(rpneq[0] >= '0' && rpneq[strlen(rpneq) -1] <= '9'))
+	{
+		std::cout << "Must begin with a number" << std::endl;
+		return(1);
+	}
+	else if(rpneq[strlen(rpneq)] == '\0' && rpneq[strlen(rpneq) -1] >= '0' && rpneq[strlen(rpneq)-1] <= '9')
+	{
+		std::cout << "Must finish with an operator" << std::endl;
+		return(1);
+	}
+	else if(rpneq[strlen(rpneq)] == '\0' && numbers != (stck.size() / 2) + 1)
+	{
+		std::cout << "Invalid equation" << std::endl;
+		return(1);
+	}
+	return(0);
 }
 
-void RPN(char **argv)
+int executernp(std::stack<int> &stck)
 {
-	if(argc % 2 == 0)
-		std::cout << "Invalid input" << std::endl;
-	else
+	int first;
+	int second;
+	int result;
+	int op;
+	first = stck.top();
+	stck.pop();
+	if(first > 9)
 	{
-		std::stack<int> stck;
-		createlist(stck, argv[1]);
-		executernp(stck);
+		std::cout << "invalid equation" << std::endl;
+		return(1);
 	}
+	second = stck.top();
+	stck.pop();
+	if(second > 9)
+	{
+		std::cout << "invalid equation" << std::endl;
+		return(1);
+	}
+	op = stck.top();
+	if(op == '+')
+		result = first + second;
+	else if(op == '-')
+		result = first - second;
+	else if(op == '*')
+		result = first * second;
+	else if(op == '/')
+		result = first / second;
+	else 
+	{
+		std::cout << "invalid equation" << std::endl;
+		return(1);
+	}
+	stck.pop();
+	while(!stck.empty())
+	{
+		first = result;
+		second = stck.top();
+		stck.pop();
+		if(second > 9)
+		{
+			std::cout << "invalid equation" << std::endl;
+			return(1);
+		}
+		op = stck.top();
+		if(op == '+')
+			result = first + second;
+		else if(op == '-')
+			result = first - second;
+		else if(op == '*')
+			result = first * second;
+		else if(op == '/')
+			result = first / second;
+		else 
+		{
+			std::cout << "invalid equation" << std::endl;
+			return(1);
+		}
+		stck.pop();
+	}
+	std::cout << "result : " << result << std::endl;
+	return(0);
+}
+
+int RPN(char **argv)
+{
+	std::stack<int> stck;
+	if(createstack(stck, argv[1]) == 1)
+		return(1);
+	if(executernp(stck) == 1)
+		return(1);
+	return(0);
 }
 
 int main (int argc, char ** argv)
 {
 	if(argc == 1)
-		std::cout << "Enter an equation in polish reverse notation" << std::endl;
-	else
-		RPN(argv);
+	{
+		std::cout << "Enter an equation in reverse polish notation" << std::endl;
+		return(1);
+	}
+	if(argc == 3)
+	{
+		std::cout << "Enter an only one reverse polish notation" << std::endl;
+		return(1);
+	}
+	if(RPN(argv) == 1)
+		return(1);
+	return(0);
 }
